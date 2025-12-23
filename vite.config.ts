ca8f1b026d8d70818a3ab48fd4ceec649ec3a5c3
xmt0114 <xmt_0114@163.com>
2025-12-23 10:51:13 +0800

    正式发布清除日志

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,26 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import sourceIdentifierPlugin from 'vite-plugin-source-identifier'
 
-const isProd = process.env.BUILD_MODE === 'prod'
-export default defineConfig({
-  plugins: [
-    react(), 
-    sourceIdentifierPlugin({
-      enabled: !isProd,
-      attributePrefix: 'data-matrix',
-      includeProps: true,
-    })
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command }) => {
+  const isProd = command === 'build'
+
+  return {
+    plugins: [
+      react(),
+      sourceIdentifierPlugin({
+        enabled: !isProd,
+        attributePrefix: 'data-matrix',
+        includeProps: true,
+      })
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    esbuild: {
+      drop: isProd ? ['console', 'debugger'] : [],
+    },
+  }
 })
 
